8cca25702fbe2777c48b483c4477a49e9c0084ac
Javi <javier@typeform.com>
2023-01-10 11:52:17 +0100

    refactor: fizzbuzz

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -1,11 +1,15 @@
 export function fizzbuzz (number:number) {
-    if (number % 15 === 0) {
+    function isDivisibleBy(divisor: number) {
+        return number % divisor === 0;
+    }
+    if (isDivisibleBy(15)) {
         return 'fizzbuzz';
     }
-    if (number % 3 === 0) {
+
+    if (isDivisibleBy(3)) {
         return 'fizz';
     }
-    if (number % 5 === 0) {
+    if (isDivisibleBy(5)) {
         return 'buzz';
     }
 
